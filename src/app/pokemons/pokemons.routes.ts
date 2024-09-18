5524c989c3e06aca1c29aa0765686b2b308d2644
Reyner <rlabradas@xetid.cu>
2024-09-18 10:59:02 -0400

    Routes example

---
+++ src/app/pokemons/pokemons.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { PokemonsComponent } from "./pokemons.component";
+
+export const POKEMONS_ROUTES:Routes=[
+    {path:'',component:PokemonsComponent}
+];
