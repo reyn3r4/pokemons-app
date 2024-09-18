1104aaf9670619d8e766c2fc3a3b17a87f000328
Reyner <rlabradas@xetid.cu>
2024-09-18 10:58:21 -0400

    Routes example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PokemonsComponent } from './pokemons/pokemons.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'pokemons',
+    //loadChildren:()=> import('./pokemons/pokemons.routes').then(m=>m.POKEMONS_ROUTES)
+    component:PokemonsComponent
+  },
+  {
+    path: '', 
+    //redirectTo: '/pokemons',pathMatch: 'prefix'
+    component:AppComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
