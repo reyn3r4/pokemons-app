9ec4e0fd857c6279cafcefba3b4c5950a588f74f
Reyner <rlabradas@xetid.cu>
2024-09-19 15:26:29 -0400

    Optimize pip

--- src/app/pipes/gen-pokemons-resume.pipe.ts
+++ src/app/pipes/gen-pokemons-resume.pipe.ts
@@ -7,7 +7,7 @@ import { Sort } from '@angular/material/sort';
 })
 export class GenPokemonsResumePipe implements PipeTransform {
   transform(pokemons: any[], sort: Sort): any[] {
-    let initials = pokemons.map(pok => {
+    /*let initials = pokemons.map(pok => {
       return { ...pok, initial: pok.name.charAt(0) };
     }).reduce((prev, cur) => {
       if (prev.length) {
@@ -18,19 +18,18 @@ export class GenPokemonsResumePipe implements PipeTransform {
         prev.push({ initial: cur.initial, count: 1 });
       }
       return prev;
+    }, []);*/
+    let initials = pokemons.reduce((prev, cur) => {
+        const i = prev.findIndex((element: any) => element.initial === cur.name.charAt(0));
+        if (i != -1) 
+          prev[i].count++;
+        else 
+          prev.push({ initial: cur.name.charAt(0), count: 1 });
+      return prev;
     }, []);
     if (sort.direction)
       initials = initials.sort(function (a: any, b: any) {
         return (a[sort.active] < b[sort.active] ? -1 : 1) * (sort.direction == 'asc' ? 1 : -1);
-        /*if (sort.active == 'initial')
-          if (sort.direction == 'asc')
-            return a.initial > b.initial ? 1 : a.initial < b.initial ? -1 : 0;
-          else return a.initial < b.initial ? 1 : a.initial > b.initial ? -1 : 0;
-        else {
-          if (sort.direction == 'asc')
-            return a.count > b.count ? 1 : a.count < b.count ? -1 : 0;
-          else return a.count < b.count ? 1 : a.count > b.count ? -1 : 0;
-        }*/
       });
     return initials;
   }
