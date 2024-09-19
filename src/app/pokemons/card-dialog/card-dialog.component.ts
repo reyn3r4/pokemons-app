4ec284f026e6098f3dcb9f716a21617fd87f5b51
Reyner Labrada <rlabradas@xetid.cu>
2024-09-19 14:42:03 -0400

    Modal style

--- src/app/pokemons/card-dialog/card-dialog.component.ts
+++ src/app/pokemons/card-dialog/card-dialog.component.ts
@@ -2,7 +2,7 @@ import { Component, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material/material.module';
 import { CardInfoComponent } from '../card-info/card-info.component';
-import { Pokemon } from 'src/app/models/pokemon.model';
+import { Pokemon, TypeColors } from 'src/app/models/pokemon.model';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-card-dialog',
@@ -12,11 +12,13 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
   styleUrls: ['./card-dialog.component.css']
 })
 export class CardDialogComponent {
+  color:string;
   constructor(
     public dialogRef: MatDialogRef<CardDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: Pokemon,
   ) {
-
+  let typeColor: { [key: string]: string }=TypeColors;
+  this.color=typeColor[this.data && this.data.types && this.data.types[0] && this.data.types[0].name ? this.data.types[0].name : 'bug'];
   }
   onNoClick(): void {
     this.dialogRef.close();
